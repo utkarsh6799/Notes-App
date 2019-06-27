52e01d8c8ee2e2a688abc5762340e466988888e0
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-06-27 12:51:05 +0530

    .gitignore fix

---
+++ app.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk')
+const getNotes = require('./notes')
+
+
+const command = process.argv[2]
+
+if(command==='add'){
+    console.log('adding note')
+}
+else if(command==='remove'){
+    console.log('removing note')
+}
+
+
+
+
+
+
+
+
+
+
+// const add = require('./utils')
+
+
+// const sum = add(4,-2)
+// console.log(sum)
