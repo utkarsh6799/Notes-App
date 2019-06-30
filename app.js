903fdccc4f15b3a45cc4d620044d41c77663384a
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-06-30 15:26:07 +0530

    add note

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes')
+const notes = require('./notes')
 
 //customize yargs
 yargs.version('1.1.0')
@@ -22,9 +22,8 @@ yargs.command({
         }
     },
     handler: function(argv){
+        notes.addNote(argv.title, argv.body)
         
-        console.log('Title:' + argv.title)
-        console.log('Body:' + argv.body)
     }
 })
 
