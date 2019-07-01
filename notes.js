b032a7e149dbfc57a4f79fecd394b55df836ee31
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-01 14:16:11 +0530

    improved add command function

--- notes.js
+++ notes.js
@@ -7,11 +7,10 @@ const getNotes = () => {
 
 const addNote =(title,body) => {
       const notes = loadNotes()
-      const duplicateNotes = notes.filter((note) => note.title === title)
-          
+      const duplicateNote = notes.find((note) => note.title === title)    
       
 
-      if(duplicateNotes.length === 0){
+      if(!duplicateNote){
         notes.push({
             title : title,
             body : body
