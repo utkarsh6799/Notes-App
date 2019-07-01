4c295e22853fc28798330e865be4e397c3b17b7f
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-01 10:59:17 +0530

    Checking of duplicate notes

--- notes.js
+++ notes.js
@@ -6,13 +6,22 @@ const getNotes = function (){
 
 const addNote = function(title,body){
       const notes = loadNotes()
-
-      notes.push({
-          title : title,
-          body : body
+      const duplicateNotes = notes.filter(function(note){
+          return note.title === title
       })
-      saveNotes(notes)
-}
+
+      if(duplicateNotes.length === 0){
+        notes.push({
+            title : title,
+            body : body
+        })
+        saveNotes(notes)
+        console.log("New Note Added!")
+      }else{
+          console.log("Note title taken!")
+      }
+
+    }
 
 const saveNotes = function(notes){
     const dataJSON = JSON.stringify(notes)
