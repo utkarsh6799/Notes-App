d48378796d37bbd67f146e1dc0dec468fa4a3de7
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-02 21:30:39 +0530

    es-6 destructuring

--- notes.js
+++ notes.js
@@ -6,6 +6,7 @@ const addNote =(title,body) => {
       const notes = loadNotes()
       const duplicateNote = notes.find((note) => note.title === title)    
       
+      debugger
 
       if(!duplicateNote){
         notes.push({
